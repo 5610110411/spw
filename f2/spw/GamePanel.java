e32a22e0c8669c09db3dce95084a7ed87fd89bb3
5610110411 <nine.exeo02@gmail.com>
2015-04-08 21:27:18 +0700

    *04*
    Changing background

--- f2/spw/GamePanel.java
+++ f2/spw/GamePanel.java
@@ -5,23 +5,33 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.io.IOException;
 
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
 	
 	private BufferedImage bi;	
+	private BufferedImage background = null;
 	Graphics2D big;
 	ArrayList<Sprite> sprites = new ArrayList<Sprite>();
+	BufferedImageLoader bg = new BufferedImageLoader();
 
 	public GamePanel() {
 		bi = new BufferedImage(400, 600, BufferedImage.TYPE_INT_ARGB);
 		big = (Graphics2D) bi.getGraphics();
-		big.setBackground(Color.BLACK);
+		background = new BufferedImage(400, 600, BufferedImage.TYPE_INT_ARGB);
+		try{
+			background = bg.loadImage("/f2/spw/Pictures/background.png");
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 
 	public void updateGameUI(GameReporter reporter){
 		big.clearRect(0, 0, 400, 600);
+		
+		Background();
 		
 		big.setColor(Color.WHITE);		
 		big.drawString(String.format("%08d", reporter.getScore()), 300, 20);
@@ -32,6 +42,10 @@ public class GamePanel extends JPanel {
 		repaint();
 	}
 
+	public void Background(){		
+		big.drawImage(background, 0, 0, 400, 600, null);
+	}
+	
 	@Override
 	public void paint(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
